90c33efd4200aae2e0ae59f05a52399f1e206dca
lewis <drakaru@gmail.com>
2017-05-24 22:02:54 +0100

    adding ability to read tweets from an account as a data source, name read from follow.txt, adding follow.txt to gitignore

--- mBot.py
+++ mBot.py
@@ -1,106 +1,134 @@
 #
-#	Markov chain bot
+#   Markov chain bot
 #
-#	
+#
 #
 import tweepy
 import collections
 import random as r
 import string
+import time
 
 
+# ----------------------------
+#   Tweepy stuff
+# ----------------------------
 
-#----------------------------
-#	Tweepy stuff
-#----------------------------
-def postTweet(s,info):
-	auth = tweepy.OAuthHandler(info['ck'], info['cs'])
-	auth.set_access_token(info['at'], info['ats'])
-	api = tweepy.API(auth)
-	statusmsg=(s);
-	try:
-		api.update_status(status=statusmsg);
-		print('Tweet Successful')
-	except Exception:
-		#Stops everything crashing if there is a connection issue.
-		print('Tweet Unsuccessful');
-
-def tweepyInit():
-	print("Initialising Tweepy...");
-	tokens=open('tokens.txt');
-	info={
-	'ck':"",
-	'cs':"",
-	'at':"",
-	'ats':""
-	}
-	consumer_key=tokens.readline();
-	consumer_secret=tokens.readline();
-	access_token=tokens.readline();
-	access_token_secret=tokens.readline();
-	tokens.close();
-	#Remove the NewLineCharacter;
-	info['ck']=consumer_key[:-1];
-	info['cs']=consumer_secret[:-1];
-	info['at']=access_token[:-1];
-	info['ats']=access_token_secret[:-1];
-	print("Complete!");
-	return info;
-#----------------------------
-
-tweepInfo=tweepyInit();
+def get_api(info):
+    auth = tweepy.OAuthHandler(info['ck'], info['cs'])
+    auth.set_access_token(info['at'], info['ats'])
+    return tweepy.API(auth)
 
 
+def post_tweet(statusmsg, info):
+    auth = tweepy.OAuthHandler(info['ck'], info['cs'])
+    auth.set_access_token(info['at'], info['ats'])
 
-f=open('data.txt');
-s=f.readlines();
-e=[];
-t=[];
+    try:
+        api.update_status(status=statusmsg)
+        print('Tweet Successful')
+    except Exception:
+        # Stops everything crashing if there is a connection issue.
+        print('Tweet Unsuccessful')
+
+
+def get_tweets(user):
+    tweets = api.user_timeline(id=user, count=20)
+    messages = []
+    for tweet in tweets:
+
+        text = get_full_tweet(tweet.id)
+        print(text)
+        if len(text.split()) <= 1:
+            continue
+        messages.append(text)
+
+    return messages
+
+
+def get_full_tweet(id):
+    return api.get_status(id, tweet_mode='extended')._json['full_text']
+
+
+def tweepy_init():
+    print("Initialising Tweepy...")
+    tokens = open('tokens.txt')
+    info = {
+        'ck': "",
+        'cs': "",
+        'at': "",
+        'ats': ""
+    }
+    consumer_key = tokens.readline()
+    consumer_secret = tokens.readline()
+    access_token = tokens.readline()
+    access_token_secret = tokens.readline()
+    tokens.close()
+    # Remove the NewLineCharacter;
+    info['ck'] = consumer_key[:-1]
+    info['cs'] = consumer_secret[:-1]
+    info['at'] = access_token[:-1]
+    info['ats'] = access_token_secret[:-1]
+    print("Complete!")
+    return info
+
+# ----------------------------
+
+tweepyInfo = tweepy_init()
+api = get_api(tweepyInfo)
+
+# f = open('data.txt')
+# s = f.readlines()
+# f.close()
+follow = open('follow.txt')
+s = get_tweets(follow.readline())
+follow.close()
+time.sleep(1)
+
+e = []
+t = []
 for i in range(len(s)):
-	t.append(s[i].split());#first word from the database
-	e.append(t[i][0]);#append it to array of first words
+    t.append(s[i].split())  # first word from the database
+    e.append(t[i][0])  # append it to array of first words
 
-#this loop works out which words are duplicate so it can begin the p structure
-d={};#define dictionary, this will hold each word and the location
+# this loop works out which words are duplicate so it can begin the p structure
+d = {}  # define dictionary, this will hold each word and the location
 for i in range(len(s)):
-	#d[e[i]]=[t[i][1]];
-	d[e[i]]=[]
-	for j,k in enumerate(e):
-		if(k==e[i]):
-			d[e[i]].append(t[j][1]);
+    # d[e[i]]=[t[i][1]];
+    d[e[i]] = []
+    for j, k in enumerate(e):
+        if k == e[i]:
+            d[e[i]].append(t[j][1])
 
-size=10;
-d0={};
+size = 10
+d0 = {}
 for i in range(len(s)):
-	for p in range(len(t[i])):
-		d0[t[i][p]]=[]
-		for j,k in enumerate(t[i]):
-			if((k==t[i][p])and(j!=len(t[i])-1)):
-				d0[t[i][p]].append(t[i][j+1]);
+    for p in range(len(t[i])):
+        d0[t[i][p]] = []
+        for j, k in enumerate(t[i]):
+            if (k == t[i][p]) and (j != len(t[i]) - 1):
+                d0[t[i][p]].append(t[i][j + 1])
 
+f0 = e[r.randint(0, len(s) - 1)]  # first word
+f1 = d[f0][r.randint(0, len(d[f0]) - 1)]  # second word
+f2 = d0[f1][r.randint(0, len(d0[f1]) - 1)]  # anything after can follow this
+output = f0 + " " + f1 + " " + f2
+op = f2
+outLen = r.randint(5, 10)
 
+end = False
 
+while not end:
+    try:
+        on = d0[op][r.randint(0, len(d0[op]) - 1)]  # output new
+        op = on  # output previous
+        output = output + " " + op
+        if len(d0[op]) == 0:
+            # output=output+".";
+            end = True
+    except:
+        #	output=output+".";
+        break
 
-f0=e[r.randint(0,len(s)-1)];#first word
-f1=d[f0][r.randint(0,len(d[f0])-1)]#second word
-f2=d0[f1][r.randint(0,len(d0[f1])-1)]#anything after can follow this
-output=f0+" "+f1+" "+f2;
-op=f2;
-outLen=r.randint(5,10);
-
-end=False;
-
-
-while end!=True:
-	try:
-		on=d0[op][r.randint(0,len(d0[op])-1)]#output new
-		op=on;#output previous
-		output=output+" "+ op;
-		if(len(d0[op])==0):
-			#output=output+".";
-			end=True;
-	except:
-	#	output=output+".";	
-		break;
-print(output);	
-postTweet(output,tweepInfo);
+print("\n\n" + output)
+#post_tweet(output, tweepyInfo)
