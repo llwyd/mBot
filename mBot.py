573d454b22bd31d7b9f9e726bf57067b362720e5
llwyd <tom@llwyd.io>
2018-05-22 17:00:15 +0100

    borked everything, rebuilding

--- mBot.py
+++ mBot.py
@@ -1,8 +1,11 @@
 #
+#
+#
 #   Markov chain bot
+#		v0.2
 #
 #
-#
+#import libraries
 import tweepy
 import random as r
 import time
@@ -13,41 +16,11 @@ import settings
 import numpy as np
 import os
 import datetime as dt
-
-#-------------------------------------------------------------
-#
-#       VARIOUS FUNCTIONS
-#
-#-------------------------------------------------------------
-
-# ----------------------------
-#   Text Filtering
-# ----------------------------
-# Filter tweets with particular keywords
-def filter_word(t, keyword):
-	for i in range(len(t)):
-		for j in range(len(t[i])):
-			if t[i][j] == keyword:
-				return True
-	return False
-
-
-# Filter tweets with mentions and hashtags
-def filter_prefix(t, prefix):
-	for i in range(len(t)):
-		for j in range(len(t[i])):
-			if t[i][j][0] == prefix:
-				return True
-	return False
-
-# ----------------------------
-#   Tweepy stuff
-# ----------------------------
-def get_api(info):
-	auth = tweepy.OAuthHandler(info['ck'], info['cs'])
-	auth.set_access_token(info['at'], info['ats'])
-	return tweepy.API(auth)
-
+import sys
+import twitter as tw
+import json
+import collections
+import pickle
 
 def post_tweet(statusmsg, info):
 	auth = tweepy.OAuthHandler(info['ck'], info['cs'])
@@ -61,88 +34,6 @@ def post_tweet(statusmsg, info):
 		print('Tweet Unsuccessful')
 
 
-# get tweets as a map of <id, message>
-def get_tweets(num):
-	if tweet_store.num() is 0:
-		tweets = api.home_timeline(count=num)
-	else:
-		tweets = api.home_timeline(count=num, since_id=list(tweet_store.tweets.keys())[0])
-
-	tweet_store.save(tweets, num)
-
-	count = 0
-
-	for tid in tqdm(list(tweet_store.tweets.keys()), desc='Retrieving Timeline'):
-
-		text = tweet_store.get(tid)
-
-		if filter_tweet(tid, text):
-			continue
-
-		# only grab the full tweet if we need it
-		# as this is slow
-		if '…' in text:
-			tweet_store.update(tid, get_full_tweet(tid))
-			if filter_tweet(tid, text):
-				continue
-
-		if settings.get_filter_links():
-			tweet_store.update(tid, " ".join(list(filter(lambda o: not re.compile(r'.*(https|http)://').search(o), tweet_store.get(tid).split()))))
-
-		count += 1
-		# print(text)
-
-	return tweet_store.tweets
-
-
-def filter_tweet(tid, text):
-
-	# if matches any patterns
-	for filterPattern in settings.get_filter_patterns():
-		if filterPattern.search(text):
-			tweet_store.remove(tid)
-			return True
-
-	# or matches any prefixes
-	split = text.split()
-
-	for filterPrefix in settings.get_filter_prefixes():
-		if filter_prefix(split, filterPrefix):
-			tweet_store.remove(tid)
-			return True
-
-	# or matches any first words
-	for filterFirstWord in settings.get_filter_first_words():
-		if split[0] == filterFirstWord:
-			tweet_store.remove(tid)
-			return True
-
-	# or is too short
-	if len(split) < settings.get_filter_min_length():
-		tweet_store.remove(tid)
-		return True
-
-	return False
-
-
-def get_full_tweet(tweet_id):
-	return api.get_status(tweet_id, tweet_mode='extended')._json['full_text']
-
-
-def tweepy_init():
-	print("Initialising Tweepy...")
-	tokens = open('tokens.txt')
-	info = {
-		'ck': tokens.readline()[:-1],
-		'cs': tokens.readline()[:-1],
-		'at': tokens.readline()[:-1],
-		'ats': tokens.readline()[:-1]
-	}
-
-	tokens.close()
-	print("Complete!")
-	return info
-
 def build_word(e,d,d0):
 	r.seed(dt.datetime.now());
 	pf0 = ""
@@ -150,10 +41,13 @@ def build_word(e,d,d0):
 	rFlag = True  # flag to check for repeat
 	while rFlag:
 		f0 = e[r.randint(0, len(e) - 1)]  # first word
-		if f0 != pf0 and len(d[f0]) > 0:
+		if len(d[f0]) > 1:
 			rFlag = False
-	pf0 = f0  # Store first word into variable so it can avoid repeats
-	f1 = d[f0][r.randint(0, len(d[f0]) - 1)]  # second word
+	rFlag=True;
+	while rFlag:
+		f1 = d[f0][r.randint(0, len(d[f0]) - 1)]  # second word
+		if len(d[f0]) > 1:
+			rFlag = False
 	# f2 = d0[f1][r.randint(0, len(d0[f1]) - 1)]  # anything after can follow this
 	output = f0 + " " + f1
 	op = f1
@@ -172,117 +66,160 @@ def build_word(e,d,d0):
 				break
 
 	# Add a full stop to the end if necessary.
-	lastchar = output[len(output) - 1]
-	print(output);
-	if lastchar != '.' or lastchar != '?' or lastchar != '!':
+	#lastchar = output[len(output) - 1]
+	lastchar=output[-1];
+	#print(output);
+	#This doesnt work, wtf
+	if ((output[-1] != '.') or (output[-1] != '?') or (output[-1] != '!')):
 		output += "."
-
+	#print(output);
 	# print("Complete!\n", flush=True)
-
 	return output
 
-#-------------------------------------------------------------
-#
-#       PROGRAM BEGINS HERE
-#
-#-------------------------------------------------------------
-tweepyInfo = tweepy_init()
-api = get_api(tweepyInfo)
 
-# ----------------------------
-# Parameters and initialisation
-# ----------------------------
-# number of tweets to read (capped at 200 by twitter)
-numTweet = settings.get_tweet_history_limit()
-# time between tweets in seconds
-delay = settings.get_tweet_frequency()
-# size of overall storage buffer
-buffSize = 1000
-# current position of buffer
-buffPos = 0
-#master list of tweets
-master = []
-# key, array of first values
-e = []
-t = []
-# define dictionary, this will hold each word and the location
-d = {}
-d0 = {}
-# Filter for removing punctuation (except sentence endings)
-puncFilter = str.maketrans('', '', '\"$%&\'()*+,-/:;<=>@[\\]‘^_`{|}~')
-#flag for enabling tweets
-active=settings.get_tweet_post();
-#flag for enabling punctuation filter
-punc=settings.get_filter_punc();
+#Initialise Tweepy
+try:
+	tweetStuff=tw.tweepy_init();
+	api = tw.get_api(tweetStuff);
+except Exception as ex:
+	print(ex);
+	sys.exit(1);
+#Set Parameters
+try:
+	# number of tweets to read (capped at 200 by twitter)
+	numTweet = settings.get_tweet_history_limit()
+	# time between tweets in seconds
+	delay = settings.get_tweet_frequency()
+	# size of overall storage buffer
+	buffSize = 1000
+	# current position of buffer
+	buffPos = 0
+	#master list of tweets
+	master = []
+	# key, array of first values
+	e = []
+	t = []
+	# define dictionary, this will hold each word and the location
+	d = {}
+	d0 = {}
+	# Filter for removing punctuation (except sentence endings)
+	puncFilter = str.maketrans('', '', '…\"$%&\'()*+,-/:;<=>@[\\]‘^_`{|}~')
+	#flag for enabling tweets
+	active=settings.get_tweet_post();
+	#flag for enabling punctuation filter
+	punc=settings.get_filter_punc();	
+except Exception as ex:
+	print(ex);
+	sys.exit(1);
 
-print("-----------------------------------------", flush=True)
-# get latest tweets
-s = list(get_tweets(numTweet).values())
-# filter punctuation
+if(os.path.isfile("misc.json")==True):
+	with open('misc.json') as data_file:
+		pos=json.load(data_file);
+		data_file.close();
+else:
+	pos={};
+	pos['id']=0;
+#get tweets
+if(pos['id']==0):
+	tweets = api.home_timeline(count=numTweet,tweet_mode='extended');
+else:
+	tweets = api.home_timeline(count=numTweet,tweet_mode='extended', since_id=pos['id']);
+#filter text and sort by ids
+latestTweets={};
+for x in tweets:
+	latestTweets[x.id]=x.full_text;
+# re-sort the tweets such that the newest appears first in the dict
+latestTweets = collections.OrderedDict(sorted(latestTweets.items(), reverse=True))
+
+#Put current keys into callable list
+keys=list(latestTweets.keys());
+
+#begin filtering
+
+#collect list to purge
+purgeList=[];
+#print(len(keys));
+for i in range(len(keys)):
+	#Filter Retweets
+	if(latestTweets[keys[i]][0:2]=="RT"):
+		purgeList.append(i);
+	#Filter direct Messages
+	elif (latestTweets[keys[i]][0]=='@'):
+		purgeList.append(i);
+	#print(i);
+#purge list
+for i in range(len(purgeList)):
+	latestTweets.pop(keys[purgeList[i]]);
+	#keys.pop(purgeList[i]);
+keys=list(latestTweets.keys());
+#for i in range(len(purgeList)):
+#	keys.pop(purgeList[i]);
+
+#Master list of tweets
+new=list(latestTweets.values());
+
+with open("database.txt","rb") as fp:
+	s=pickle.load(fp);
+
+for i in range(len(new)):
+	s.append(new[i]);
+
+#Remove textless image only tweets;
+purgeList=[];
+for i in range(len(s)):
+	if(s[i][0:5]=="https"):
+		purgeList.append(i);
+for i in range(len(purgeList)-1,-1,-1):
+	s.pop(purgeList[i]);
+#punctuation filter
 if(punc==True):
 	for i in range(len(s)):
-		s[i] = s[i].translate(puncFilter)
-#check if firstWords database exists
-if(os.path.isfile("firstWords.npy")==True):
-	#load the database
-	e=np.load("firstWords.npy");
-	#typecase to list for consistency
-	e=list(e);
-else:
-	#if not then create empty list
-	e=[];
+		s[i]=s[i].translate(puncFilter);
+#Split array of words
+for i in range(len(s)):
+	t.append(s[i].split());
+	if(t[i][-1][0:5]=="https"):
+		t[i].pop(-1);
+	if(t[i][-1][0:5]=="https"):
+		t[i].pop(-1);
+#Remove single word tweets
+purgeList=[];
+for i in range(len(t)):
+	if(len(t[i])<2):
+		purgeList.append(i);
+for i in range(len(purgeList)):
+	t.pop(purgeList[i]);
 #--------------------------------------
 #   begin filtering of firstWords
 #--------------------------------------
-for i in range(len(s)):
-	if s[i] is None:
-		continue
-	dup=False
+for i in range(len(t)):
+	dup=False;
 	for j in range(len(e)):
-		if(e[j]==s[i].split()[0]):
+		if(t[i][0]==e[j]):
 			dup=True;
 			break;
-	if(dup!=True):
-		e.append(s[i].split()[0]);
-	# array of separated words for latest tweets
-	t.append(s[i].split());
-
+	if(dup==False):
+		e.append(t[i][0]);
 #--------------------------------------
-#   begin filtering of secondWords
+#	Filter second words
 #--------------------------------------
-#check if database exists
-if(os.path.isfile("secondWords.npy")==True):
-	#load the database
-	d=np.load("secondWords.npy").item();
-else:
-	#if not then create empty dictionary
-	d={};
 #check if the first word already exists in the dictionary
 for i in range(len(e)):
 	if((e[i] in d))==False:
 		d[e[i]]=[];
 #Create dictionary of first words and second words
-for i in range(len(s)):
-	if s[i] is None:
-		continue
+for i in range(len(t)):
 	dup=False
+	#print(i);
 	for j in range(len(d[t[i][0]])):
 		if(d[t[i][0]][j])==t[i][1]:
 			dup=True;
 			break;
 	if(dup!=True):
 		d[t[i][0]].append(t[i][1]);
-
 #--------------------------------------
-#   begin filtering of otherWords
+#	Other Words
 #--------------------------------------
-#check if database exists
-if(os.path.isfile("otherWords.npy")==True):
-	#load the database
-	d0=np.load("otherWords.npy").item();
-else:
-	#if not then create empty dictionary
-	d0={};
 #check if word already exists
 for i in range(len(s)):
 	if s[i] is None:
@@ -302,25 +239,13 @@ for i in range(len(s)):
 				break;
 		if(dup!=True):
 			d0[t[i][j-1]].append(t[i][j]);
- 
-
 output = build_word(e,d,d0)
-while len(output) > 140:
+while len(output) > 280:
 	output = build_word(e,d,d0)
+print(output+"\n");
 
+post_tweet(output,tweetStuff);
 
-#Store databases
-np.save("firstWords.npy",e);
-np.save("secondWords.npy",d);
-np.save("otherWords.npy",d0);
+with open ("database.txt","wb") as fp:
+	pickle.dump(s,fp);
 
-print("*****************************************", flush=True)
-print("" + output + "", flush=True)
-print("*****************************************", flush=True)
-
-if(active==True):
-	post_tweet(output, tweepyInfo)
-else:
-	debugPost=input("Would you like to post this?(y/n)");
-	if(debugPost=='y'):
-		post_tweet(output, tweepyInfo);
