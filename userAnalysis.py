36f5759cf4654004581682db259b0cfdb5410306
llwyd <tom@llwyd.io>
2018-07-02 19:09:02 +0100

    basic regex for analysing user profiles to count tweets

--- userAnalysis.py
+++ userAnalysis.py
@@ -13,7 +13,8 @@ import settings
 import twitter as tw
 from collections import Counter
 from tqdm import tqdm
-
+import matplotlib.pyplot as plt
+import numpy as np
 #
 #
 #
@@ -42,10 +43,34 @@ try:
     del users
     users=list(count.keys());
     users.sort();
+    
+
+    with open('dataAnalysis.json','r') as f:
+        k=json.load(f);
+    keyWords=k['keywords'];
+    regex=[]
+    for i in range(len(keyWords)):
+    	regex.append(re.compile(keyWords[i]))
+    tweetCount={}
+    for i in range(len(keyWords)):
+    	tweetCount[keyWords[i]]=0;
     desc=[]
+    print("Harvesting profile data...")
     for i in tqdm(range(len(users))):
     	desc.append(api.get_user(users[i]).description)
-    # Fancy regex here to harvest and count party affilication, then compare with user tweet count
+    	desc[i]=desc[i].lower();
+    	for j in range(len(regex)):
+    		r=regex[j].search(desc[i]);
+    		if(r is not None):
+    			tweetCount[keyWords[j]]=tweetCount[keyWords[j]]+count[users[i]]
+
+    #plot bar graph
+    #x=np.arange(len(keywords));
+    #y=[]
+    #for i in range(len(x)):
+    #	y.append(tweetCount[keyWords[j]]);
+    #plt.bar(x,y)
+    #plt.show();
 
 except Exception:
     traceback.print_exc()
